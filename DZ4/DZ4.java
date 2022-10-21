c53d1d5f5c9061d8046c8c904a652384cea6fbd0
Tanya <tanyahalopova@gmail.com>
2022-10-21 13:27:59 +0500

    m

--- DZ4/DZ4.java
+++ DZ4/DZ4.java
@@ -5,9 +5,17 @@ public class DZ4{
         film a=new film("Harry Potter",2001,"Daniel Radcliffe");
         a.getname();
         System.out.println(a.getyear());
+        a.setyear(2025);
+        System.out.println(a.getyear());
+        favoritfilm b = new favoritfilm("Harry Potter",2001,"Daniel Radcliffe");
+        b.getname();
+        System.out.println(b.getyear());
+        b.getnambeofvies();
+        b.setnambeofvies(4);
+        b.getnambeofvies();
     }
 } 
-class film{
+class film {
     String name;
     private int year = 0; 
     String actor;
@@ -24,8 +32,29 @@ class film{
         return this.year;
     }
     public void setyear(int year){
-        if(year < 2022){
+        if(0<year && year< 2022){
             this.year = year;
         }
+        else{
+            this.year = 0;
+        }
+    }
+}
+class favoritfilm extends film {
+    int nambeofvies;
+    public favoritfilm (String name, int year, String actor){
+        this(name,year,actor,3);
+    }
+    public favoritfilm (String name, int year, String actor,int nambeofvies){
+        super(name,year,actor);
+        setnambeofvies(nambeofvies);
+    }
+    public void setnambeofvies(int nambeofvies){
+            if(3<=nambeofvies && nambeofvies <=100){
+                this.nambeofvies = nambeofvies;
+            }
+    }
+    public void getnambeofvies(){
+            System.out.println(nambeofvies);
     }
 }
