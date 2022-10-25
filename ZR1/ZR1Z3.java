786c237d5c72b2ceeadccbab12c88c86b797fbae
Tanya <tanyahalopova@gmail.com>
2022-10-25 15:24:37 +0500

    m

--- ZR1/ZR1Z3.java
+++ ZR1/ZR1Z3.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
 public class ZR1Z3{
-    public static void main(String[] args[]){
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int x = 1;
-        int p = 0;
+        int p = -2;
         int col = 0;
         int max = 0;
         while (x!=0){
             x = scanner.nextInt();
-            if (x==p ){
+            if (x==p){
                 col+=1;
                 if (col>max){
                     max=col;
                 }
             }
+            else{
+                col=0;
+            }
             p=x;
-            col=0;
         }
         System.out.println(max);
     }
