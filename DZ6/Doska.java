2b6824e98ba02809df92711c1bfbf76c14dae9f6
Tanya <tanyahalopova@gmail.com>
2022-12-06 02:36:36 +0500

    m

---
+++ DZ6/Doska.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class  Doska{
+    private int n = 0;
+    public Doska(n){
+        if (n>10){
+            System.err.println()
+        }
+        this.n=n;
+    }
+    public int getn(){
+        return n;
+    }
+}
